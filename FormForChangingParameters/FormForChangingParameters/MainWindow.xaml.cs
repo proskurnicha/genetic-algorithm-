cec01f241d1f2ff3cf90431a452d25a2c7a0b703
Nataliia Proskurnicha <natalia13071998@mail.ru>
2019-03-25 11:12:07 +0200

    first commit

---
+++ FormForChangingParameters/FormForChangingParameters/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FormForChangingParameters
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string arg = Iterations.Text + " " + Cities.Text + " " +Mutation.Text;
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.WorkingDirectory = @"D:\Genethic_Algorithm\genetic_algorithm";
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.Arguments = @"/c python main.py " + arg;
+            p.Start();
+
+        }
+    }
+}
